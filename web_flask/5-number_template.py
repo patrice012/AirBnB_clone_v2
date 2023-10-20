88cbb425a7784c52ae5d52dcf14e0c021907a005
patrice02 <patricedev02@gmail.com>
2023-10-20 20:49:44 +0000

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+'''
+Script that starts a Flask web application
+
+Listening on 0.0.0.0, port 5000
+Use the option strict_slashes=False in route definition
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+'''
+
+from flask import Flask, render_template
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Print Hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Print hbnb'''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''
+    Print argument value
+    '''
+    new_text = re.sub('_', ' ', text)
+    return f'C {new_text}'
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """display Python """
+    new_text = re.sub('_', ' ', text)
+    return f'Python {new_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Print n only if it's a number'''
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
