71f78c9450b36f2d0c2e868a16b3173aa2cbd8bc
patrice02 <patricedev02@gmail.com>
2023-10-20 20:34:54 +0000

    add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+'''
+Script that starts a Flask web application
+
+Listening on 0.0.0.0, port 5000
+Use the option strict_slashes=False in route definition
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+'''
+
+from flask import Flask
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Print Hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Print hbnb'''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    '''
+    Print argument value
+    '''
+    new_text = re.sub('_', ' ', text)
+    return f'C {new_text}'
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """display Python """
+    new_text = re.sub('_', ' ', text)
+    return f'Python {new_text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Print n only if it's a number'''
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
